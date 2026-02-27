2e0e7973e95f39f203b62b400ab7b1166177ee5c
josInholland <jos.meuleman@inholland.nl>
2026-02-27 11:34:53 +0100

    Update FRLogger.h

--- FRLoggerTemplate/FRLogger.h
+++ FRLoggerTemplate/FRLogger.h
@@ -1,4 +1,5 @@
 // Class for logging data to an SD card
+// DO NOT CHANGE THIS FILE
 //
 // 2024-03-07, Jos Meuleman & Tim van Cuylenborg, Inholland Aeronautical & Precision Engineering, The Netherlands
 
