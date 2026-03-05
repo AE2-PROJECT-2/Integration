2fff5383401f5f49f3136caea3f0353aacac5d87
josInholland <jos.meuleman@inholland.nl>
2026-03-05 14:44:41 +0100

    Create customfunctions.h

---
+++ FRServoControlTemplate/customfunctions.h
@@ -0,0 +1,92 @@
+#ifndef CUSTOMFUNCTIONS_H
+#define CUSTOMFUNCTIONS_H
+
+//---------------------------------------------------------------------------------------------------------
+// FUNCTIONS
+// Here the custom functions are defined
+//---------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------------
+// Function that prints an error to the serial port and makes the RGBLED red
+//---------------------------------------------------------------------------------------------------------
+void Error(String errorMessage) {
+  Serial.println(errorMessage);
+  myLed.SetColor(RED);
+}
+
+//---------------------------------------------------------------------------------------------------------
+// Function that gives the state of a tristate switch: LOSTATE (-1), MIDSTATE (0), HIGHSTATE (1)
+// Note that the PPM Signals are such that they produce low values when the switch is physically up/high
+//---------------------------------------------------------------------------------------------------------
+triStateSwitch GetChannelTriState(int channelValue){
+  if (channelValue < PPMTHRESHLOW) {
+    return HISTATE;
+  } else if (channelValue < PPMTHRESHHIGH) {
+    return MIDSTATE;
+  } else {
+    return LOSTATE;
+  }
+}
+
+//---------------------------------------------------------------------------------------------------------
+// Function that controls the leading gear and hatch, based on the switch state
+//---------------------------------------------------------------------------------------------------------
+
+void HandleLandingGearSwitch(int channelValue){
+  // landingGearSwitchState can have the state LOSTATE (-1), MIDSTATE (0), or HISTATE (1)
+  // the function GetChannelTriState returns one of these three states depending on the value of the channel
+  landingGearSwitchState =  GetChannelTriState(channelValue);
+
+  if (landingGearSwitchState < landingGearSwitchStatePrev) {
+    // towards rectracted state
+    if (landingGearSwitchState == MIDSTATE) {
+      // So it was HISTATE, now pull in the gear
+      servoTargetPos[SERVOLANDINGGEAR] = SERVOLANDINGGEARPOSRETRACTED;
+      Serial.println("Retract gear");
+    } else {
+      // So it was in MIDSTATE, now close the hatch
+      servoTargetPos[SERVOLANDINGHATCH] = SERVOLANDINGHATCHPOSCLOSED;
+      Serial.println("Close hatch");
+    }
+  }
+  if (landingGearSwitchState > landingGearSwitchStatePrev) {
+    // towards extended state
+    if (landingGearSwitchState == MIDSTATE) {
+      // So it was LOSTATE, now open the hatch
+      servoTargetPos[SERVOLANDINGHATCH] = SERVOLANDINGHATCHPOSOPEN;
+      Serial.println("Open hatch");
+    } else {
+      // So it was in MIDSTATE, now extend the gear
+      servoTargetPos[SERVOLANDINGGEAR] = SERVOLANDINGGEARPOSEXTENDED;
+      Serial.println("Extend gear");
+    }
+  }
+  landingGearSwitchStatePrev = landingGearSwitchState; //update the previous state for the next loop
+
+}
+
+
+//---------------------------------------------------------------------------------------------------------
+// Function that controls the servos to their setpoints with limited speed
+//---------------------------------------------------------------------------------------------------------
+void UpdateServos(){
+  for (int i = 0; i < NUMBEROFSERVOS; i++) {
+    // Calculate the differrence between target and current position
+    float posError = servoTargetPos[i] - servoActualPos[i];
+    float maxStep = MAXSERVOSPEEDDEGS[i]*LOOPTIMESERVOMS/1000.0; //maximum step a servo can make in a loopcycle based on maximum speed
+    if (posError > maxStep) {
+      // if the difference is larger than the maximum step size, limit the step size
+      posError = maxStep;
+    }
+    if (posError < -maxStep) {
+      // same, but for negative values
+      posError = -maxStep;
+    }
+    servoActualPos[i] = servoActualPos[i]+posError;
+
+    // Write the rounded off setpoint to the servo motor
+    myServo[i].write(int(servoActualPos[i]));
+  }
+}
+
+#endif  //CUSTOMFUNCTIONS_H
